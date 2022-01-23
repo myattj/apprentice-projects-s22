5883dc795928d53acec375c729cba98f0c035cf8
Joshua Myatt <myattj@ad.unc.edu>
2022-01-23 15:44:14 -0500

    finished

--- p03-flag-refactoring/p03-flag-refactor/GameViewModel.swift
+++ p03-flag-refactoring/p03-flag-refactor/GameViewModel.swift
@@ -7,4 +7,36 @@
 
 import Foundation
 
-#warning("Add GameViewModel class in this file")
+
+class GameViewModel: ObservableObject {
+    @Published var countries = CountryList.countries.shuffled().prefix(3)
+    @Published var correctAnswerIndex = Int.random(in: 0...2)
+    @Published var score = 0
+
+    @Published var showingAlert = false
+    @Published var alertTitle = ""
+    
+    
+    
+    var targetCountry: Country {
+        return countries[correctAnswerIndex]
+    }
+    
+    
+    
+    
+    
+    
+    func correct(){
+        alertTitle = "Correct"
+        score += 1
+    }
+    
+    
+    func wrong(country: Country){
+        alertTitle = "Wrong! Thats the flag of \(country)"
+    }
+}
+
+
+
